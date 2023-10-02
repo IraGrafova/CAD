a72d46d47fbf5d47973e365ec29c03ec8886c14c
IraGrafova <i.grafova@ntc-hitech.ru>
2023-10-02 15:07:54 +0300

    port

--- CAD/webpack.config.js
+++ CAD/webpack.config.js
@@ -1,63 +1,64 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: { main: './src/index.js' },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.[hash].js',
-            publicPath: ''
+  entry: { main: "./src/index.js" },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.[hash].js",
+    publicPath: "",
+  },
+  mode: "development",
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
+    compress: true,
+    port: 3001,
+
+    open: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: "babel-loader",
+        exclude: "/node_modules/",
       },
-      mode: 'development',
-      devServer: {
-        static: {
-          directory: path.resolve(__dirname, 'build'),
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "images/[name].[hash][ext]",
         },
-        open: true,
       },
-      module: {
-        rules: [
-            {
-              test: /\.js$/,
-              use: 'babel-loader',
-              exclude: '/node_modules/',
+      {
+        test: /\.(woff|woff2|ttf|eot|otf)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "fonts/[name].[hash][ext]",
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 1,
             },
-            {
-              test: /\.(png|svg|jpg|jpeg|gif)$/,
-              type: 'asset/resource',
-              generator: {
-                filename: 'images/[name].[hash][ext]'
-              }
-            },
-            {
-            test: /\.(woff|woff2|ttf|eot|otf)$/,
-            type: 'asset/resource',
-            generator: {
-              filename: 'fonts/[name].[hash][ext]'
-            }
           },
-              {
-                test: /\.css$/,
-                use: [
-                  MiniCssExtractPlugin.loader,
-                  {
-                    loader: 'css-loader',
-                    options: {
-                      importLoaders: 1
-                    }
-                  },
-                  'postcss-loader'
-                ]
-              },
-          ],
+          "postcss-loader",
+        ],
       },
-      plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-          }),
-          new CleanWebpackPlugin(),
-          new MiniCssExtractPlugin(),
-      ] ,
-   }
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+  ],
+};
