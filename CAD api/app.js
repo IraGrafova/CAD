722377aac22fe520732876855af387396d6d03d8
IraGrafova <i.grafova@ntc-hitech.ru>
2023-10-04 23:36:43 +0300

    points and line

--- CAD api/app.js
+++ CAD api/app.js
@@ -1,77 +1,60 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const router = require('express').Router();
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const router = require("express").Router();
+const cors = require("cors");
 
-const Cone = require('./models/cone');
+const Cone = require("./models/cone");
 
+const app = express();
 
-const app = express(); 
-
-mongoose.connect('mongodb://127.0.0.1/CAD', {
+mongoose.connect("mongodb://127.0.0.1/CAD", {
   useNewUrlParser: true,
-  
 });
 
 app.use(bodyParser.json()); // для собирания JSON-формата
 app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
 
-
-
-
-
-
 const doTriangulation = (req, res) => {
-    console.log(req.body)
+  console.log(req.body);
 
-//     X, Y, Z coordinates of a node:
-// A: {0, 0, H}, where H is a cone height
-// Pi : {R * cos (2π * i / N), R * sin (2π * i / N), 0} , i=0,N-1 – see fig.2
+  //     X, Y, Z coordinates of a node:
+  // A: {0, 0, H}, where H is a cone height
+  // Pi : {R * cos (2π * i / N), R * sin (2π * i / N), 0} , i=0,N-1 – see fig.2
 
-const H = req.body.height;
-const R = req.body.radius;
-const N = req.body.segments;
+  const H = Number(req.body.height);
+  const R = req.body.radius;
+  const N = req.body.segments;
 
-console.log(H, R, N)
+  console.log(Number(H));
 
-const array = [];
-for (let i = 0; i < N; i++) {
+  const array = [0, 0, H];
+  for (let i = 0; i < N; i++) {
+    //каждый треугольник состоит из 3 точек координат
+    // точка Pi состоит из косинуса синуса и 0 (x,y,z)
+    // const Pi = {(R)*(Math.cos(2*Math.PI * i / N)), (R) * (Math.sin(2*Math.PI * i / N)), 0}
 
-  //каждый треугольник состоит из 3 точек координат
-  // точка Pi состоит из косинуса синуса и 0 (x,y,z)
-  // const Pi = {(R)*(Math.cos(2*Math.PI * i / N)), (R) * (Math.sin(2*Math.PI * i / N)), 0}
+    array.push(
+      R * Math.cos((2 * Math.PI * i) / N),
+      R * Math.sin((2 * Math.PI * i) / N),
+      0
+    );
+  }
 
-  const Px = (R)*(Math.cos(2*Math.PI * i / N));
-  const Py = (R) * (Math.sin(2*Math.PI * i / N));
-  const Pz = 0;
+  Cone.create(req.body)
+    .then(() => res.status(200).send(array))
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-  const coordinates = {Px, Py, Pz}
-  console.log(coordinates)
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3001",
+  })
+);
 
-  array.push(coordinates)
-
-  console.log(array)
-  
-  console.log('!!!!!!')
-}
-
-  Cone.create(req.body)  
-  .then(() => res.status(200).send(array))
-  .catch(err => {
-    console.log(err)
-  });
-
-    
-   
-}
-
-app.use(cors({
-  credentials: true,
-  origin: 'http://localhost:3001',
- 
-}));
-
-app.use(router.post('/', doTriangulation)); //post
+app.use(router.post("/", doTriangulation)); //post
 
 app.listen(3000);
--- CAD/src/index.js
+++ CAD/src/index.js
@@ -1,140 +1,90 @@
-import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from "three";
 
-import './index.css';
-import {doTriangulation} from './utils/Api.js';
+import "./index.css";
+import { doTriangulation } from "./utils/Api.js";
 
-const buttonSubmit = document.querySelector('.form__submit');
-const form = document.querySelector('.form');
-const inputHeight = document.querySelector('.form__input_height');
-const inputRadius = document.querySelector('.form__input_radius');
-const inputSegments = document.querySelector('.form__input_segments');
-
-let data = {};
-
-
- 
-
-
-function submitData(evt) {
-    evt.preventDefault();
-    
-data.height = inputHeight.value;
-data.radius = inputRadius.value;
-data.segments = inputSegments.value;
-    
-    doTriangulation(data).then((array) => {
-console.log(array)
-
-
-
-
-
-
-
-
-
-
-
-
-
-    })
-}
-
-form.addEventListener('submit', submitData)
-
+const buttonSubmit = document.querySelector(".form__submit");
+const form = document.querySelector(".form");
+const inputHeight = document.querySelector(".form__input_height");
+const inputRadius = document.querySelector(".form__input_radius");
+const inputSegments = document.querySelector(".form__input_segments");
 
 //Добавляем сцену:
-const scene = new THREE.Scene(); 
+const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x282c34);
 
 //Добавляем перспективную камеру:
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 ); 
-// const camera = new THREE.OrthographicCamera(-1, 1, 1.5, -1.5, 1, 10);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
 
-// camera.position.set(0, 0, 5); // перемещение объекта  (горизонталь, вертикаль, глубина)
-
+// // Чем больше z значение, тем дальше расположен объект от экрана
+camera.position.set(0, -8, 20); // перемещение объекта  (горизонталь, вертикаль, глубина)
 
 //Создаем объект рендера, устанавливаем его размер в соответствии с размером видимой области и добавляем его на страницу
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 // Берем элемент DOM и прикрепляем renderer.domElement к нему
-document.body.appendChild( renderer.domElement );
+document.body.appendChild(renderer.domElement);
 
+let data = {};
 
-//для движения камеры
-// Добавляем управление, устанавливаем как цель тот же DOM элемент
-// let controls = new OrbitControls(camera, document.getElementById('threejs'));
-// controls.target.set(0, 0, 0);
-// controls.rotateSpeed = 0.5;
-// controls.update();
+function submitData(evt) {
+  evt.preventDefault();
 
+  data.height = inputHeight.value;
+  data.radius = inputRadius.value;
+  data.segments = inputSegments.value;
 
+  doTriangulation(data).then((array) => {
+    scene.clear();
+    console.log(array);
 
+    const geometry = new THREE.BufferGeometry();
+    const vertices = new Float32Array(array);
+    const arrayLines = [];
+    for (let i = 1; i < array.length / 3; i++) {
+      arrayLines.push(i);
+    }
 
-//Для создания конуса сначала задаем геометрию:
-// const geometry = new THREE.ConeGeometry( 5, 20, 10 );  //(радиус, высота,  число граней)
+    for (let i = 0; i < array.length / 3; i++) {
+      arrayLines.push(0, i);
+    }
+    console.log(arrayLines);
 
+    const lines = arrayLines.push(Number(1));
+    console.log(lines);
 
+    const indices = arrayLines;
 
-const geometry = new THREE.BufferGeometry();
-const vertices = new Float32Array([
-	new THREE.Vector3(-10, 10, 0),
-	new THREE.Vector3(-10, -10, 0),
-	new THREE.Vector3(10, -10, 0)
-]);
-geometry.addAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    geometry.setIndex(indices);
+    geometry.setAttribute("position", new THREE.BufferAttribute(vertices, 3));
 
+    //Задаем матерал
+    const material = new THREE.MeshBasicMaterial({ color: 0xa9a9a9 }); //wireframe: true, emissive: 0x111111,
 
+    const cone = new THREE.Line(geometry, material);
 
-//Задаем матерал
-const material = new THREE.MeshBasicMaterial( { color: 0xA9A9A9, } ); //wireframe: true, emissive: 0x111111,
+    scene.add(cone);
 
-// Теперь нам нужен объект Mesh, который принимает геометрию, и применяет к нему материал:
-const cone = new THREE.Mesh( geometry, material );
-
-// cone.scale.x = 0.8;  // изменение размера
-// cone.scale.y = 0.8;
-// cone.scale.z = 0.8;
-
-scene.add( cone );
-
-
-// Чем больше значение, тем дальше расположен объект от экрана
-camera.position.z = 20;
-
-// // Добавляем освещение, устанавливаем его и добавляем на сцену
-// const frontSpot = new THREE.SpotLight(0xeeeece);
-// const frontSpot2 = new THREE.SpotLight(0xddddce);
-
-// frontSpot.position.set(1000, 1000, 1000);
-// frontSpot2.position.set(-500, -500, -500);
-
-// scene.add(frontSpot);
-// scene.add(frontSpot2);
-
-const clock = new THREE.Clock();
-
-const animate = function () {
-
-    const elapsedTime = clock.getElapsedTime();
-
-    requestAnimationFrame(animate);
-
-    // cone.rotation.x += 0.01;  // вращение объекта
-    // cone.rotation.y += 0.01;
-    // cone.rotation.z += 0.01;
-
-    cone.rotation.z = Math.PI*-0.25;  // quaternion
-
-    // camera.lookAt(new THREE.Vector3(0, 0, 20))
-
-    renderer.render(scene, camera);
+    inputHeight.value = "";
+    inputRadius.value = "";
+    inputSegments.value = "";
+  });
 }
 
-// const frontSpot = new THREE.SpotLight(0xeeeece);
-// frontSpot.position.set(1000, 1000, 1000);
-// scene.add(frontSpot);
+form.addEventListener("submit", submitData);
+
+const animate = function () {
+  requestAnimationFrame(animate);
+
+  // cone.rotation.z += 0.01;
+
+  renderer.render(scene, camera);
+};
 
 animate();
